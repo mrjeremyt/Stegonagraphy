a310908b995ab3d3075784ac602b60d3c8ada9a5
mrjeremyt <jthompson2214@gmail.com>
2014-10-21 11:32:50 -0500

    input is now set up

--- src/Stegonography.java
+++ src/Stegonography.java
@@ -1,17 +1,36 @@
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import javax.imageio.ImageIO;
 
 public class Stegonography {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		Boolean enkode = true;
+		if(!args[0].substring(1).equals("E"))
+			enkode = false;
+			
         BufferedImage img = null;
         try {
-            img = ImageIO.read(new File(args[0]));
+            img = ImageIO.read(new File(args[1]));
         } catch (IOException e) {
-
+        	System.out.println("Invalid image file");
         }
+        
+        Path path = Paths.get(args[2]);
+        
+		//read the entire file into this byte array
+		byte [] data = Files.readAllBytes(path);
+
+		//create a stream on the array
+		ByteArrayInputStream is = new ByteArrayInputStream(data);
+        
+        
         int height = img.getHeight();
         int width = img.getWidth();
 
