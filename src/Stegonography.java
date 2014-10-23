104afe47ed8b7dd07ddf93820357ca05a0a8c4bc
majisto <majisto@gmail.com>
2014-10-22 19:05:05 -0500

    Starting to get file size and set up the bit creation algorithm.

--- src/Stegonography.java
+++ src/Stegonography.java
@@ -10,7 +10,10 @@ import javax.imageio.ImageIO;
 import javax.imageio.stream.ImageOutputStream;
 
 public class Stegonography {
-
+	
+	public static int height;
+	public static int width;
+	
 	public static void main(String[] args) throws IOException {
 		Boolean enkode = true;
 		if(!args[0].substring(1).equals("E"))
@@ -22,7 +25,6 @@ public class Stegonography {
         } catch (IOException e) {
         	System.out.println("Invalid image file");
         }
-      
         Path path = Paths.get(args[2]);
         
 		//read the entire file into this byte array
@@ -30,6 +32,7 @@ public class Stegonography {
 
 		//create a stream on the array
 		ByteArrayInputStream is = new ByteArrayInputStream(data);
+		System.out.println(is.available());
         
 		String img_info[] = Paths.get(args[1]).getFileName().toString().split("\\.");
 		String out_img_name = img_info[0] + "-steg." + img_info[1]; 
@@ -41,30 +44,34 @@ public class Stegonography {
         boolean x = ImageIO.write(temp, img_info[1].toString(), f);       
 		
 		
-        int height = img.getHeight();
-        int width = img.getWidth();
+        height = img.getHeight();
+        width = img.getWidth();
 
         int amountPixel = 0;
 
         System.out.println(height  + "  " +  width + " " + img.getRGB(30, 30));
 
         
-/*        for(int i = 0; i < width; i++){
-        	for(int j = 0; j < height; j++){
-        		int pixel = img.getRGB(i, j);
-                int alpha = (pixel >> 24) & 0xFF;
-                int r = (pixel >> 16) & 0xFF;
-                int g = (pixel >> 8) & 0xFF;
-                int b = pixel & 0xFF;
-                System.out.println(alpha + " " + r + " " + g + " " + b);
-                amountPixel++;
-        	}
-        }
-        System.out.println("Full number of pixels: " + amountPixel);*/
+//        for(int i = 0; i < width; i++){
+//        	for(int j = 0; j < height; j++){
+//        		int pixel = img.getRGB(i, j);
+//                int alpha = (pixel >> 24) & 0xFF;
+//                int r = (pixel >> 16) & 0xFF;
+//                int g = (pixel >> 8) & 0xFF;
+//                int b = pixel & 0xFF;
+//                System.out.println(alpha + " " + r + " " + g + " " + b);
+//                amountPixel++;
+//        	}
+//        }
+//        System.out.println("Full number of pixels: " + amountPixel);
         
         
 	// This prints the image height and width and a specific pixel. 
 
 	}
 
+	private boolean enough_bits ()
+	{
+		return false;
+	}
 }
