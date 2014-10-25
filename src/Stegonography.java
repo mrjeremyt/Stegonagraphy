a45bfc1ae8b6aa46f9de5ec8c6ee08d3d63e1a80
majisto <majisto@gmail.com>
2014-10-24 19:11:41 -0500

    Cleaned up code.

--- src/Stegonography.java
+++ src/Stegonography.java
@@ -1,7 +1,5 @@
 import java.awt.Color;
 import java.awt.image.BufferedImage;
-import java.awt.image.ColorModel;
-import java.awt.image.WritableRaster;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
@@ -36,8 +34,6 @@ public class Stegonography {
         	System.out.println("Invalid image file");
         	System.exit(-1);
         }    
-              
-
 
 		//prints the image statistics no matter whether enkode or dekode
         System.out.println("Input image file: " + Paths.get(args[1]).getFileName().toString() + 
@@ -61,9 +57,6 @@ public class Stegonography {
 			
 			LinkedList<Byte> the_bits = full_of_byte(is);
 			Iterator<Byte> it = the_bits.iterator();
-			boolean do_one_more = false;
-			boolean upper = true;
-			int when_to_write = 0;
 			int num_times_for_zero = 0;
 			
 			outerloop:
@@ -76,11 +69,9 @@ public class Stegonography {
 	                
 	        		if(it.hasNext())
 	        		{
-//	        			print_rgb(pixel, true);
 	        			byte test_byte = it.next();
-	        			System.out.println(Integer.toBinaryString((test_byte & 0xFF) + 0x10).substring(1)); //Converts byte to binary string.
+//	        			System.out.println(Integer.toBinaryString((test_byte & 0xFF) + 0x10).substring(1)); //Converts byte to binary string.
 		                int newrgb = newrgb(r, g, b, test_byte);
-//		                print_rgb(newrgb, false);
 		                
 		                temp.setRGB(i, j, newrgb);
 	        		}
@@ -99,7 +90,6 @@ public class Stegonography {
 
 	        	}
 	        }
-//			System.out.println("Wtf is this alpha crap?  Do you even haz one bro? Answer: " + cm.hasAlpha());
 	        ImageIO.write(temp, img_info[1].toString(), output_file);
 		}else{
 			File output_file = new File(Paths.get(args[2]).getFileName().toString() + "-out");
@@ -111,32 +101,22 @@ public class Stegonography {
 	        for(int i = 0; i < width; i++){
 	        	for(int j = 0; j < height; j++)
 	        	{
-//	        		Color c = new Color(img.getRGB(i, j), true);
-//	        		int r = c.getRed();
-//	        		int g = c.getGreen();
-//	        		int b = c.getBlue();
-	        			                
 	                if(current_iter < 3)
 	                {
 		        		Color c = new Color(img.getRGB(i, j), true);
 		        		int r = c.getRed();
 		        		int g = c.getGreen();
 		        		int b = c.getBlue();
-		        		print_rgb(img.getRGB(i, j), false);
 	                	buffer = decode_rgb(r, g, b, current_iter, buffer);
-	                	System.out.println("Upper buffer: " + buffer);
 	                	current_iter++;
 	                }
 	                else
 	                {
 	                	j--;
 	                	current_iter = 0;
-//	                	buffer = decode_rgb(r, g, b, current_iter, buffer);
-	                	System.out.println("Full Buffer: " + buffer);
 	                	if(buffer.byteValue() != 0)
 	                	{
 	                		fs.write(new byte[]{buffer.byteValue()});
-//	                		System.out.println("Final buffer to write: " + Byte.toString(buffer));
 	                		buffer = 0;
 	                	}
 	                	else  		break outerloop;
@@ -183,7 +163,6 @@ public class Stegonography {
 			
 			if ((b %2) == 1) 	buffer = (byte) (buffer | (1 << 6));
 			else	buffer = (byte) (buffer | (0 << 6));
-//			System.out.println("Upper buffer: " + buffer);
 		}
 		else if (pos == 1){			
 			if ((r %2) == 1) 	buffer = (byte) (buffer | (1 << 5));
@@ -194,7 +173,6 @@ public class Stegonography {
 			
 			if ((b %2) == 1) 	buffer = (byte) (buffer | (1 << 3));
 			else	buffer = (byte) (buffer | (0 << 3));
-//			System.out.println("Lower buffer: " + (buffer & 0x7F));
 		}
 		else if (pos == 2)
 		{
@@ -243,16 +221,13 @@ public class Stegonography {
 				byte c = (byte) (current_byte & 0x7);
 				x.add(a); x.add(b); x.add(c);
 				num_bytes_written++;
-				System.out.println("Error: Message is larger than the image. Message has been truncated.");
 			}
+			System.out.println("Error: Message is larger than the image. Message has been truncated.");
 			return x;
 		}
-		
-
 	}
 	
 	private static int newrgb(int r, int g, int b, Byte by){
-		System.out.println(by);
 		byte one= (byte) ((by.byteValue() >> 2) & 0x1);
 		byte two = (byte) ((by.byteValue() >> 1) & 0x1);
 		byte three = (byte) ((by.byteValue() >> 0) & 0x1);
@@ -270,8 +245,7 @@ public class Stegonography {
 		if((byte)(b % 2) != three){
 			if(b == 0) 	b+=1;
 			else b-=1;
-		}
-		
+		}	
 		
 		int newrgb = 0;
 		newrgb = (b | newrgb);
@@ -279,60 +253,4 @@ public class Stegonography {
 		newrgb = (r << 16) | newrgb;
 		return newrgb;
 	}
-	
-	
-//	private static ArrayList<Integer> fill_array (ArrayList<Integer> bytes, int test_byte) throws IOException
-//	{
-//		System.out.println("Test byte: " + test_byte);
-//		bytes.add((test_byte & 0x7));
-//		test_byte >>= 3;
-//		bytes.add((test_byte & 0x7));
-//		test_byte >>= 3;
-//		bytes.add((test_byte & 0x7));
-//		return bytes;
-//	}
-	
-//	private static int recon_char (int [] bytes)
-//	{
-//		int recon = 0;
-//		recon |= bytes[0];
-//		recon |= bytes[1] << 3;
-//		recon |= bytes[2] << 6;
-//		return recon;
-//	}
-	
-//	//The below code is horrible, and I feel ashamed I wrote it...
-//	//you should be ashamed. This is sheet
-//	//I am ashamed...
-//	private static int get_3bits (ByteArrayInputStream is)
-//	{
-//		int newbit = 0;
-//		if (bits_in_buffer > 3)
-//		{
-//			System.out.println("Buffer in bits > 3 begins at: " + buffer);
-//			bits_in_buffer -= 3;
-//			int threebits = buffer & 0x7;
-//			buffer >>= 3;
-//			System.out.println("Threebits in bits >3 is: " + threebits);
-//			System.out.println("Bits in buffer is: " + bits_in_buffer);
-//			return threebits;
-//		}
-//		else
-//		{
-//			if ((newbit = is.read()) !=-1)
-//			{
-//				System.out.println("Newbit is : " + newbit);
-//				buffer = (newbit << bits_in_buffer) | buffer;
-//				System.out.println("Buffer after insertion is: " + buffer);
-//				int threebit = buffer & 0x7;
-//				buffer >>= 3;
-//				System.out.println("Three bit is : " + threebit);
-//				System.out.println("Buffer after threebit removal is: " + buffer);
-//				bits_in_buffer += 5;
-//				return threebit;
-//			}
-//			else
-//				return my_eof;
-//		}
-//	}
 }
